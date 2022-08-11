925ab00c53ad461bb7edb2f319395d11825c3784
diyarose775 <diyarose1988@gmail.com>
2022-08-11 16:04:39 -0400

    first commit

---
+++ Student List/src/array/ArrayDemo.java
@@ -0,0 +1,28 @@
+package array;
+
+public class ArrayDemo {
+
+	
+	
+
+	
+
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ Student List/src/array1/ArrayList.java
@@ -0,0 +1,45 @@
+package array1;
+
+public class ArrayList {
+
+	public static void main(String[] args) {
+
+		//declaration and instantiation
+	String student[] = new String [15];
+	student[0] = "Archimedes";
+	student[1] = "Pythagoras";
+	student[2] = "Katrina";
+	student[3] = "Madhuri";	
+	student[4] = "Sharukh";	
+	student[5] = "Kajal";	
+	student[6] = "Rani";	
+	student[7] = "Bibek";
+	student[8] = "Emran";
+	student[9] = "vidya Balan";
+	student[10] = "karina";
+	student[11] = "Sakib";
+	student[12] = "Salman";
+	student[13] = "Rajesh";
+	student[14] = "Sharmila";
+	
+	for (int i = 0; i < student.length; i++) {
+		
+		System.out.println(student [i]);
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+}
